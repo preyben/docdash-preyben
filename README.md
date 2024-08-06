7c7a67a0f3957d689f19bbec192a7a86daa7c335
preyben <605120504@qq.com>
2024-08-06 16:50:30 +0800

    调整模板

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-1、需要安装jsdoc
-2、package.json设置：
+## 模板编写人 徐鸿远
+
+## 1、需要安装jsdoc
+## 2、package.json设置：
 vue3 + vite:
 {
   "scripts": {
@@ -18,13 +20,13 @@ vue2 + cli:
   },
 }
 
-3、拷贝jsdoc.json 放到自己项目和package.json相同层级的目录，然后修改相关配置
+## 3、拷贝jsdoc.json 放到自己项目和package.json相同层级的目录，然后修改相关配置
 
-4、注释参考
+## 4、注释参考
 js文件注释参考/example/test.js
 vue文件注释参考/example/test.vue
 
-5、打包自己项目的组件库的设置
+## 5、打包自己项目的组件库的设置
 vite.config.js:
 // 假设没有其他配置
 let build = {};
@@ -62,7 +64,7 @@ export default defineConfig({
 vue.config.js:
 parallel设置为false, 如果需要生产为true则动态判断parallel = !process.argv.find(i => i.includes('jsdoc/lib')), 主要判断命令是否包含目标路径
 
-6、./src/umd.js配置
+## 6、./src/umd.js配置
 vue3:
 import Test from '@/component/test.vue';
 const components = [Test];
--- example/test.vue
+++ example/test.vue
@@ -4,17 +4,28 @@
 </template>
 <script setup>
 /**
- * @module UI组件/测试
+ * @module UI组件
  */
 /**
+ * @summary 示范组件
  * @function pb-test
  * @param {string} value - 通过v-model绑定
+ * @param {Object} config - 配置对象
+ * @param {string} [config.color="#fff"] - 颜色
+ * @param {string} [config.size] - 大小
+ * @param {method} setValue -
+ * 设置值的方法
+ * 参数(value)
+ * @listens change(value) 值更新事件
+ * @description
+ * 自定义描述 
  * @example <caption>使用案例</caption>
  *   <pb-test v-model="text"/>
  * @example <caption>使用案例-script</caption>
  * {
  *   // 案例名后加-script, 顺序不能搞错, 会作为option api的配置项
  *   // 使用script时，案例的内容开头需自己对齐
+ *   // 案例名后加-mobile会以手机iframe形式显示，注意加script需要这样写使用案例-mobile-script
  *   data() {
  *     return {
  *       text: '测试输入框',
